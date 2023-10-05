7917f5c352dc3346c432b27a0405c2f45ce5a79f
Teju-Anil <127083204+Teju-Anil@users.noreply.github.com>
2023-10-05 14:55:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	 var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+
+    // Check if both fields are filled
+    if (firstName !== "" && lastName !== "") {
+        // Show an alert with the first and last name
+        alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    } else {
+        alert("Please fill out both first and last name fields.");
+    }
 
 }
